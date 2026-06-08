0f4cdfeb8b318f818998717dbcb82de303ef258c
gabrielabdala8-cloud <gabrielabdala8@gmail.com>
2026-06-08 15:46:30 -0400

    Delete page.js

--- page.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import { processData } from "../lib/parseData";
-import Dashboard from "./Dashboard";
-
-export default function Page() {
-  const csvPath = path.join(process.cwd(), "data", "quotes.csv");
-  const { rawRows } = processData(csvPath, "Synergie Canada");
-  return <Dashboard rawRows={rawRows} />;
-}
